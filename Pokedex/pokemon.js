ade7dfa7fba80044258dbbeae9e8b473cbed4c67
matteobuccarella-cpu <matteo.buccarella@yahoo.it>
2026-04-01 00:00:54 +0200

    Update pokemon.js

--- Pokedex/pokemon.js
+++ Pokedex/pokemon.js
@@ -6,12 +6,27 @@ const allPokemon = [
     image: "images/flexape.jpeg", // 👈 minuscolo
     stats: {
       hp: 50,
-      attack: 70,
-      defense: 50,
-      sp_attack: 40,
+      attack: 65,
+      defense: 40,
+      sp_attack: 35,
       sp_defense: 40,
-      speed: 65
+      speed: 50
     },
     description: "Mena le mani con allegria seguendo il ritmo della canzone che ha nelle cuffie"
+  }
+{
+    id: 2,
+    name: "chefurious", // 👈 minuscolo
+    types: ["lotta", "fuoco"],
+    image: "images/chefurious.jpeg", // 👈 minuscolo
+    stats: {
+      hp: 60,
+      attack: 80,
+      defense: 50,
+      sp_attack: 35,
+      sp_defense: 50,
+      speed: 50
+    },
+    description: "Adora cucinare e arrabbiarsi, una combo pericolosa con delle padelle roventi in mano"
   }
 ];
